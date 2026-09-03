e355239206de8e8b59cf108e85749024c4c4bb5e
Yu Sheng <ays03@bu.edu>
2026-09-03 10:48:22 -0400

    Change to mdx

--- apps/labs/astro.config.mjs
+++ apps/labs/astro.config.mjs
@@ -6,10 +6,6 @@ import remarkMath from 'remark-math';
 
 export default defineConfig({
   output: 'static',
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
   integrations: [
     react(),
     mdx({
